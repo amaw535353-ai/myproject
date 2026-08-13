e1ca80521c9a697587daad03cacd2c41eccb6ea9
lahcennh3-jpg <lahcennh3@gmail.com>
2026-08-13 19:12:59 +0000

    P3-B: add default agent budget wrapper

---
+++ aegis/agent/default_budgeted_runner.py
@@ -0,0 +1,6 @@
+from aegis.agent.execution_budget import P2G_POLICY_VERSION
+from aegis.agent.graph import AgentRunner
+
+
+class DefaultBudgetedAgentRunner(AgentRunner):
+    policy_version = P2G_POLICY_VERSION
